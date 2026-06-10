36946b75529378c7a7493b5ea09bec765f628b8c
CoreyPickett <pickettcorey23@gmail.com>
2026-06-10 16:10:55 +1000

    Updated get_results
    Added error if no Kaggle token found. Added comments through script.

--- src/data/get_results.py
+++ src/data/get_results.py
@@ -1,14 +1,24 @@
+## Helper script which downloads the 'International football results from 1872 to 2026' Dataset from Kaggle using Kaggle API
+
+# Import Libraries
 from dotenv import load_dotenv
 import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 
+# Load .env file
 load_dotenv()
 
-os.environ["KAGGLE_API_TOKEN"] = os.getenv("KAGGLE_API_TOKEN")
+# Ensure token is available
+token = os.getenv("KAGGLE_API_TOKEN")
+if not token:
+    raise ValueError("KAGGLE_API_TOKEN not found in .env")
 
+# Set environment variable for Kaggle API
+os.environ["KAGGLE_API_TOKEN"] = token
+
+# Authenticate
 api = KaggleApi()
 api.authenticate()
-
 
 # Download dataset
 api.dataset_download_files(
